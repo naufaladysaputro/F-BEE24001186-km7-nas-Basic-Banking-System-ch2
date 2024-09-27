9f7f2f2b2783175a8630afef927cffb6c10d9c98
naufaladysaputro <157557765+naufaladysaputro@users.noreply.github.com>
2024-09-28 04:53:25 +0700

    subclass savingAccount and main function to display the menu

--- bank_account.js
+++ bank_account.js
@@ -43,7 +43,51 @@ class bankAccount {
       }
 }
 
-// Membuat objek bank
-const bank = new bankAccount();   // Membuat objek baru bernama bank dari kelas bankAccount.
+// Definisi kelas SavingsAccount (subclass)
+class SavingsAccount extends bankAccount {
+    constructor(saldo) {
+      super(saldo);
+    }
+  
+    showSaldo() {
+      console.log(`Saldo Anda saat ini: Rp.${this.saldo}`);
+    }
+  }
+
+  // Fungsi untuk menjalankan program
+async function main() {
+    const savingsAccount = new SavingsAccount(0);
+    let isRunning = true;
+  
+    while (isRunning) {
+      const pilihan = await savingsAccount._getUserInput(
+        "\nPilih tindakan:\n1. Cek Saldo\n2. Deposit\n3. Withdraw\n4. Keluar\nPilih (1-4): "
+      );
+  
+      switch (pilihan) {
+        case 1:
+          savingsAccount.showSaldo();
+          break;
+        case 2:
+          
+          break;
+        case 3:
+
+          break;
+        case 4:
+          console.log("Terima kasih! Program berakhir.");
+          isRunning = false;
+          break;
+        default:
+          console.log("Pilihan tidak valid, silakan coba lagi.");
+          break;
+      }
+    }
+  
+    rl.close(); // Menutup readline ketika selesai
+  }
+  
+  // Menjalankan program utama
+  main();
 
 
