81d96e8a3db28d9989984433bbf055642f1f603a
naufaladysaputro <157557765+naufaladysaputro@users.noreply.github.com>
2024-09-28 03:55:57 +0700

    remove: function tambahSaldo and kurangSaldo

--- bank_account.js
+++ bank_account.js
@@ -4,27 +4,7 @@ class bankAccount {
       this.saldo = parseFloat(localStorage.getItem("saldo")) || 0; // Inisialisasi properti saldo dengan nilai yang diambil dari localStorage dengan kunci "saldo"
     }
   
-    tambahSaldo() {   // Menambahkan saldo ke rekening.
-      const inputTambahJumlah = parseFloat(window.prompt("Silakan masukkan nominal saldo yang ingin ditambahkan:")); // Memasukkan nominal saldo yang ingin ditambahkan melalui window.prompt
-      if (!isNaN(inputTambahJumlah) && inputTambahJumlah > 0) {  // Memeriksa inputan lebih besar dari 0
-        this.saldo += inputTambahJumlah;
-        this.simpanSaldo();
-      } else {
-        window.alert("Silakan masukkan nominal saldo yang sesuai.");  // Menggunakan window.alert untuk menampilkan pesan kesalahan
-      }
-      return;
-    }
   
-    kurangiSaldo() {  // Mengurangi saldo dari rekening.
-      const inputKurangJumlah = parseFloat(window.prompt("Silakan masukkan nominal saldo yang ingin dikurangkan:")); // Memasukkan nominal saldo yang ingin dikurangkan melalui window.prompt
-      if (!isNaN(inputKurangJumlah) && inputKurangJumlah > 0 && inputKurangJumlah <= this.saldo) {  // Memeriksa inputan valid dan mencukupi saldo
-        this.saldo -= inputKurangJumlah;
-        this.simpanSaldo();
-      } else {
-        window.alert("Silakan masukkan nominal saldo yang sesuai atau pastikan saldo mencukupi."); // Menggunakan window.alert untuk menampilkan pesan kesalahan
-      }
-      return;
-    }
   
     simpanSaldo() {  // Untuk menyimpan saldo ke localStorage.
       localStorage.setItem("saldo", this.saldo.toString());  // Nilai saldo diubah ke tipe data string menggunakan toString().
